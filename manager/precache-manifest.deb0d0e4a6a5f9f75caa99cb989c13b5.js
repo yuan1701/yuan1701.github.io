ab94d0afe72c82d69ce593d226a9e02b3298a1df
yuanyun <1349281070@qq.com>
2020-01-14 14:01:13 +0800

    添加后台管理系统

---
+++ manager/precache-manifest.deb0d0e4a6a5f9f75caa99cb989c13b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e15a28bf5576c2f55caa521fca5ced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc24df9dd96e364efae1",
+    "url": "/static/css/2.806183df.chunk.css"
+  },
+  {
+    "revision": "dc24df9dd96e364efae1",
+    "url": "/static/js/2.958d6bd0.chunk.js"
+  },
+  {
+    "revision": "8a564417767dd82c4942b240501b3361",
+    "url": "/static/js/2.958d6bd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b880a1cce7631ebdf82",
+    "url": "/static/js/main.c6370b41.chunk.js"
+  },
+  {
+    "revision": "3dcbfae8abba7f924df2",
+    "url": "/static/js/runtime-main.b3800c1f.js"
+  }
+]);
